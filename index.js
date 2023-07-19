b16417dfe935f232c72d560ffedb9e88fa3f82d3
niksi99 <104139232+niksi99@users.noreply.github.com>
2023-07-19 17:43:04 +0200

    kraj

---
+++ index.js
@@ -0,0 +1,16 @@
+import WebSocket, {WebSocketServer} from "ws";
+
+const wss = new WebSocketServer({port: 3001});
+wss.on('connection', function connection(ws) {
+    ws.on('message', function message(message) {
+        const data = JSON.parse(message);
+
+        if(data.type === 'message') {
+            wss.clients.forEach(client => {
+                if(client !== ws && client.readyState === WebSocket.OPEN) {
+                    client.send(JSON.stringify({ type: 'message', data: data.data}));
+                } 
+            })
+        }
+    });
+});
